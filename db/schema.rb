fec9c2c9e41f6414df5944e99cfdc741b1a5d1ae
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-03-14 11:29:43 -0300

    Criando a tela de boas vindas

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2025_03_13_194214) do
+
+  create_table "coins", force: :cascade do |t|
+    t.string "description"
+    t.string "acronym"
+    t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
